786b34e748b1157cd1083b1fd370fd1253110381
Ike Lin <ikelin@gmail.com>
2012-03-14 15:47:00 -0700

    remove duplicate function

--- src/resource/ResourceEventRenderer.js
+++ src/resource/ResourceEventRenderer.js
@@ -619,22 +619,3 @@ function ResourceEventRenderer() {
 	
 
 }
-
-
-function countForwardSegs(levels) {
-    var i, j, k, level, segForward, segBack;
-    for (i=levels.length-1; i>0; i--) {
-        level = levels[i];
-        for (j=0; j<level.length; j++) {
-            segForward = level[j];
-            for (k=0; k<levels[i-1].length; k++) {
-                segBack = levels[i-1][k];
-                if (segsCollide(segForward, segBack)) {
-                    segBack.forward = Math.max(segBack.forward||0, (segForward.forward||0)+1);
-                }
-            }
-        }
-    }
-}
-
-
