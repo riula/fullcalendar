b6810b3d0a6e757a99518eaac6ac4f747fbcc0f0
Ike Lin <ikelin@gmail.com>
2012-05-03 14:14:19 -0700

    added better sample events dates

--- demos/json-resource-events.php
+++ demos/json-resource-events.php
@@ -2,35 +2,36 @@
 
 	$year = date('Y');
 	$month = date('m');
+	$day = date('d');
 
 	echo json_encode(array(
 	
 		array(
 			'id' => 111,
 			'title' => "Event1",
-			'start' => "$year-$month-10",
-			'url' => "http://yahoo.com/",
+			'start' => "$year-$month-$day",
+			'url' => "http://google.com/",
             'resourceId' => 1
 		),
 		
 		array(
 			'id' => 222,
 			'title' => "Event2",
-			'start' => "$year-$month-19T08:00:00Z",
-			'end' => "$year-$month-19T12:00:00Z",
+			'start' => "$year-$month-${day}T08:00:00Z",
+			'end' => "$year-$month-${day}T12:00:00Z",
             'allDay' => false,
-			'url' => "http://yahoo.com/",
+			'url' => "http://google.com/",
             'resourceId' => 2
-		)/*,
+		),
 
         array(
 			'id' => 333,
-			'title' => "Event2",
-			'start' => "$year-$month-19T14:30:00Z",
-			'end' => "$year-$month-19T16:00:00Z",
+			'title' => "Event3",
+			'start' => "$year-$month-${day}T14:30:00Z",
+			'end' => "$year-$month-${day}T16:00:00Z",
             'allDay' => false,
             'resourceId' => 3
-		)*/
+		)
 	
 	));
 
