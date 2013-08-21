9b0bf80015b8d33b6557a234f8a06af9b497443b
Romeo Iula <romeo@24hrassistant.com>
2013-08-21 12:17:34 -0600

    Missed conflict, resolved now

--- src/resource/ResourceDayView.js
+++ src/resource/ResourceDayView.js
@@ -2,39 +2,6 @@
 fcViews.resourceDay = ResourceDayView;
 
 function ResourceDayView(element, calendar) {
-<<<<<<< HEAD
-	var t = this;
-	
-	
-	// exports
-	t.render = render;
-	
-	
-	// imports
-	ResourceView.call(t, element, calendar, 'resourceDay');
-	var opt = t.opt;
-	var renderResource = t.renderResource;
-	var formatDate = calendar.formatDate;
-	
-	
-	
-	function render(date, delta) {
-		if (delta) {
-			addDays(date, delta);
-			if (!opt('weekends')) {
-				skipWeekend(date, delta < 0 ? -1 : 1);
-			}
-		}
-		var start = cloneDate(date, true);
-		var end = addDays(cloneDate(start), 1);
-		t.title = formatDate(date, opt('titleFormat'));
-		t.start = t.visStart = start;
-		t.end = t.visEnd = end;
-		renderResource();
-	}
-	
-
-=======
     var t = this;
 	
 	
@@ -64,5 +31,4 @@ function ResourceDayView(element, calendar) {
         t.end = t.visEnd = end;
         renderResource();
     }
->>>>>>> 2013-08_upgrade
 }
